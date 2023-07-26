a4d5fde56afe37bf6af37878628de9dd49ef4c84
Mateo-de-la-Torre <mateo.delatorre@gmail.com>
2023-07-25 22:37:44 -0300

    Agregamos todos los comandos

--- hellogit.js
+++ hellogit.js
@@ -1,1 +1,15 @@
-print("new Hello git"); // git init
+print("new Hello git whith changes");
+
+
+
+// git init: iniciamos git
+// git add: para añadirlo y q git lo tenca en cueta
+// git commit: para sacarle una foto y se guarde por si tenemos q volver atras
+
+// git status: para ver el estado de cada fichero
+
+// git log: muestra todos los commit                                                 
+// git tree: muestra todos los commit con diseño
+
+// git cheackout: para volver al codigo del ultimo commit
+// git diff: te muestra las diferencias de tu codigo actual con las del ultimo commit 

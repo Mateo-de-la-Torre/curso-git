84f2c73426b9ddeecc368895370c797ccd85567d
Mateo-de-la-Torre <mateo.delatorre@gmail.com>
2023-07-25 23:16:09 -0300

    6to commit

--- hellogit.js
+++ hellogit.js
@@ -14,3 +14,9 @@ print("new Hello git whith changes");
 // git cheackout: para volver al codigo del ultimo commit
 // git diff: te muestra las diferencias de tu codigo actual con las del ultimo commit 
 // git checkout id: si ponemos el id del commit, podemos poner el head en un commit anterior 
+
+// git reset --hard id: para posicionar el main y el head en un commit anterior y q no se muesten
+//                      lo posteriores o volver de nuevo poniendo el id del ultimo
+
+// git reflog: es como el log pero te muestra todos los commit por mas de haber hecho el reset --hard
+// git tag: le damos una etiqueta al commit
